411125778db36513606c9d1351720d09547be5ce
Alexsander Chernenko <letmeexist95@gmail.com>
2023-07-08 22:56:38 +0300

    Added get methods for width and height

--- ImageFormats/ImageFormat.cpp
+++ ImageFormats/ImageFormat.cpp
@@ -1,27 +1,53 @@
 #include "ImageFormat.hpp"
 
 ImageFormat::ImageFormat(ImageFormat&& imageFormat)
-    : m_Width{ imageFormat.m_Width }, m_Height{ imageFormat.m_Height }, m_Data{ std::move(imageFormat.m_Data) }
+    : m_Width{ imageFormat.m_Width }, m_Height{ imageFormat.m_Height }, 
+        m_Data{ std::move(imageFormat.m_Data) }
 {}
 
 ImageFormat::ImageFormat(const ScreenResolution width, const ScreenResolution height)
-    : m_Width{ width }, m_Height{ height }, m_Data(m_Height, ImageFormat::DataRow(m_Width))
+    : m_Width{ width }, m_Height{ height }, 
+        m_Data(m_Height, ImageFormat::DataRow(m_Width))
 {}
 
-ImageFormat::ImageFormat(const ScreenResolution width, const ScreenResolution height, const DataStorage& data)
+ImageFormat::ImageFormat(const ScreenResolution width, const ScreenResolution height, 
+        const DataStorage& data)
     : m_Width{ width }, m_Height{ height }, m_Data{ data }
 {}
 
-ImageFormat::ImageFormat(const ScreenResolution width, const ScreenResolution height, DataStorage&& data)
+ImageFormat::ImageFormat(const ScreenResolution width, const ScreenResolution height, 
+        DataStorage&& data)
     : m_Width{ width }, m_Height{ height }, m_Data{ std::move(data) }
 {}
 
-ImageFormat::Byte& ImageFormat::GetByte(const std::size_t height, const std::size_t width) noexcept(false)
+ImageFormat::ScreenResolution& ImageFormat::GetWidth() noexcept
+{
+    return m_Width;
+}
+
+const ImageFormat::ScreenResolution& ImageFormat::GetWidth() const noexcept
+{
+    return const_cast<ImageFormat*>(this)->GetWidth();
+}
+
+ImageFormat::ScreenResolution& ImageFormat::GetHeight() noexcept
+{
+    return m_Height;
+}
+
+const ImageFormat::ScreenResolution& ImageFormat::GetHeight() const noexcept
+{
+    return const_cast<ImageFormat*>(this)->GetHeight();
+}
+
+ImageFormat::Byte& ImageFormat::GetByte(const std::size_t height, 
+        const std::size_t width) noexcept(false)
 {
     return m_Data.at(height).at(width);
 }
 
-const ImageFormat::Byte& ImageFormat::GetByte(const std::size_t height, const std::size_t width) const noexcept(false)
+const ImageFormat::Byte& ImageFormat::GetByte(const std::size_t height, 
+        const std::size_t width) const noexcept(false)
 {
     return const_cast<ImageFormat*>(this)->GetByte(height, width);
 }
@@ -31,4 +57,7 @@ ImageFormat& ImageFormat::operator = (ImageFormat&& imageFormat)
     m_Height = imageFormat.m_Height;
     m_Width = imageFormat.m_Width;
     m_Data = std::move(imageFormat.m_Data);
+
+    return *this;
 }
+
--- ImageFormats/ImageFormat.hpp
+++ ImageFormats/ImageFormat.hpp
@@ -11,23 +11,32 @@ public:
     using DataRow = std::vector<Byte>;
     using DataStorage = std::vector<DataRow>; 
 
-public:
+public: 
+    ImageFormat() = default;
     ImageFormat(const ImageFormat&) = default;
     ImageFormat(ImageFormat&& imageFormat);
     
     ImageFormat(const ScreenResolution width, const ScreenResolution height);
-    ImageFormat(const ScreenResolution width, const ScreenResolution height, const DataStorage& data);
-    ImageFormat(const ScreenResolution width, const ScreenResolution height, DataStorage&& data);
+    ImageFormat(const ScreenResolution width, const ScreenResolution height, 
+            const DataStorage& data);
+    ImageFormat(const ScreenResolution width, const ScreenResolution height, 
+            DataStorage&& data);
     
     virtual ~ImageFormat() = default;
 
+    const ScreenResolution& GetWidth() const noexcept;
+    ScreenResolution& GetWidth() noexcept;
+    
+    const ScreenResolution& GetHeight() const noexcept;
+    ScreenResolution& GetHeight() noexcept;
+    
     Byte& GetByte(const std::size_t height, const std::size_t width) noexcept(false);
     const Byte& GetByte(const std::size_t height, const std::size_t width) const noexcept(false);
 
     ImageFormat& operator = (const ImageFormat&) = default;
     ImageFormat& operator = (ImageFormat&& imageFormat);
 
-private:
+protected:
     ScreenResolution m_Width;
     ScreenResolution m_Height;
     DataStorage m_Data;
