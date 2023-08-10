2ae9ee2a695d797267147a084b8eb781669dc9cc
Alexsander Chernenko <letmeexist95@gmail.com>
2023-08-10 19:20:31 +0300

    Made pixel POD and got rid of excess methods

--- Pixel/Pixel.hpp
+++ Pixel/Pixel.hpp
@@ -6,38 +6,11 @@
 class Pixel
 {
 public:
-    using Byte = std::uint16_t;
-    using ColorValues = std::tuple<Byte, Byte, Byte>;
+    using ColorValue = std::uint16_t;
 
 public:
-    Pixel() = default;
-    Pixel(const Pixel&) = default;
-    Pixel(Pixel&&) = default;
-
-    Pixel(const Byte red, const Byte green, const Byte blue);
-
-    ~Pixel() = default;
-
-    const Byte& GetRed() const noexcept;
-    Byte& GetRed() noexcept;
-
-    const Byte& GetGreen() const noexcept;
-    Byte& GetGreen() noexcept;
-    
-    const Byte& GetBlue() const noexcept;
-    Byte& GetBlue() noexcept;
-
-    const ColorValues GetColorValues() const noexcept;
-    ColorValues GetColorValues() noexcept;
-
-    Pixel& operator = (const Pixel&) = default;
-    Pixel& operator = (Pixel&&) = default;
-
-    friend std::ifstream& operator >> (std::ifstream& stream, Pixel& pixel);
-
-private:
-    Byte m_Red{};
-    Byte m_Green{};
-    Byte m_Blue{};
+    ColorValue Red{};
+    ColorValue Green{};
+    ColorValue Blue{};
 };
 
