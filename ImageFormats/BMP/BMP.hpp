7a5b15dd6f43f0281e976716cb5a220b653763ad
Alexsander Chernenko <letmeexist95@gmail.com>
2023-08-16 13:07:09 +0300

    Added ColorTable for bmp structure

--- ImageFormats/BMP/BMP.hpp
+++ ImageFormats/BMP/BMP.hpp
@@ -7,13 +7,13 @@ class BMP : public ImageFormat
 public:
     BMP() = default;
     BMP(const BMP&) = default;
-    BMP(BMP&& ppm);
-    
-    BMP(const ImageFormat& image);
-    BMP(ImageFormat&& image);
+    BMP(BMP&& bmp) = default;
     
     ~BMP() = default;
 
+    BMP& operator = (const BMP&) = default;
+    BMP& operator = (BMP&&) = default;
+    
 public:
     // Bitmap file header
     std::string HeaderField{};
@@ -22,15 +22,15 @@ public:
     std::uint16_t DataOffset{};
 
     // DIB (bitmap information) header
-    std::uint32_t CoreHeaderSize{};
-    std::uint32_t BitmapWidth{};
-    std::uint32_t BitmapHeight{};
+    std::uint32_t InfoHeaderSize{};
     std::uint16_t ColorPlanesNumber{};
     std::uint16_t BitsPerPixel{};
     std::uint32_t CompressionMethod{};
     std::uint32_t ImageSize{};
     std::uint32_t HorizontalResolution{};
     std::uint32_t VertivalResolution{};
-    std::uint32_t ColorPaletterNumber{};
+    std::uint32_t ColorPaletteNumber{};
     std::uint32_t ImportantColorsNumber{};
+
+    ImageFormat::DataStorage ColorTable{};
 };
